5dfb7db1c15e297874241fc3f30667ed6f28c21a
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-27 21:57:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,20 +1,12 @@
 def find_min_in_nested_arrays(src)
-  nested = []
-  
-  count = 0
-  while count < src.length
-    
-    inner_count = 0
-    while inner_count < src[count].length
-  
-      src[count].min 
-  
-      nested << src[count].min
-          count += 1
-
+ nested = []
+    count = 0
+    while count < src.length
+        inner_count = 0
+        src[count].sort!
+        nested << src[count][0]
+        count += 1
     end
-  end
-  
-  return nested
+    nested
 
 end
